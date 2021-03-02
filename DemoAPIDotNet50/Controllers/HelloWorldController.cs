46484a383b195017f59b3788e6144a76975a9ca7
Matt Hersh <Matt.Hersh@ascensus.com>
2021-03-02 15:18:13 -0500

    Added Hello World Endpoint

---
+++ DemoAPIDotNet50/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoAPIDotNet50.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HelloWorldController : ControllerBase
+    {       
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
